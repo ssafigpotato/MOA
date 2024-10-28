6a51d6aa6987150da32b9b6f3c421b6c0e928147
김윤홍 <yunhongkim123@gmail.com>
2024-10-28 20:27:38 +0900

    :sparkles:[Feat] kakao OAuth2.0 빌드중..

---
+++ a602/src/main/java/com/MOA/backend/global/auth/OAuth2/dto/OAuth2Attribute.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4b66cf9f4f0a61d151c43f0742b836d428e99ed7332a43fe3458e2ac620d17e5
+size 1655
---
+++ a602/src/main/java/com/MOA/backend/global/auth/jwt/dto/JwtProperties.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d63caf33aa58c67427d1e7955077920fd80fff2c46009cc51acd2b5c8d85f038
+size 349
