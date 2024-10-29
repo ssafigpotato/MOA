a5f3e386587b976ccff1ed91c3f58832c2289077
inception010203 <inception010203@gmail.com>
2024-10-29 18:02:06 +0900

    :bug:[Fix] #S11P31A602-152 #done Prettier 화살표함수 항상 괄호로 감싸도록 수정
    Former-commit-id: 547f8dbba6e0675c387964008453a026529b664e

--- moa/.prettierrc.js
+++ moa/.prettierrc.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:394f4c3c9e546b3b7d7ebe83d1453e3966b8558c706a2eec7b8931ee06769565
-size 452
+oid sha256:e80f9b59515037e3d426afd0ffe43465e995f9ba3a87346d98544a07877a7611
+size 453
