9db1976a6cba63b15af6d93b9ecad349174908da
김윤홍 <yunhongkim123@gmail.com>
2024-10-29 14:45:45 +0900

    :sparkles:[Feat] backend 브랜치 합치기
    Former-commit-id: 84dfeeb13a486b73c4ea9293ac5a796a20443b2e

---
+++ backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6736bc92f66018683f527110332a9660811cccaf7dbfb26240fbbc9dccb2a355
+size 77
---
+++ backend/src/main/java/com/MOA/backend/global/auth/OAuth2/dto/OAuth2Attribute.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d21e75d5d8388c30507d3e9430477dfd9d90d4327f7d57a998b8e9b46bf3c81
+size 1906
