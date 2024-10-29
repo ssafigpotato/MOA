6df24464e55bd5d4a8cb13e08fa251c2ef9aa2fd
inception010203 <inception010203@gmail.com>
2024-10-29 17:05:43 +0900

    :sparkles:[Feat] #S11P31A602-150 #in-progress EsLint에 Prettier 설정 추가

--- moa/.eslintrc.js
+++ moa/.eslintrc.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ff98abdbdd1e0118e1bfed78e3c99630afd428ae1b54753debe9d649413d828
-size 1356
+oid sha256:db27ea4f6ef373601ca412f9b38caf31bd6ca3512b0cb9841ef199ef5732f043
+size 1515
