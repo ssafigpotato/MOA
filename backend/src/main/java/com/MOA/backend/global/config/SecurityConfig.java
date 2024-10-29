515a13f93a2485f08a083292ec94f7815db560d5
김윤홍 <yunhongkim123@gmail.com>
2024-10-29 17:50:43 +0900

    ✨[Feat] #S11P31A602-190 #in-progress login 구현
    Former-commit-id: 92ae19e46a8d30200d4c9d29dca78b632e16557d

--- backend/src/main/java/com/MOA/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/MOA/backend/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba187bb5d93517104f7edf253fc3c102ce9588dbcd56512c5d3d6d757785f4bc
-size 343
+oid sha256:576ef37f063e44db02430b676db1b52b14bd0236d91cfd673d8bd0cf7e6586d0
+size 967
