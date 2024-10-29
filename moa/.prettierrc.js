a3b445fffe714413643b68058b0ef0973253368f
inception010203 <inception010203@gmail.com>
2024-10-29 17:54:20 +0900

    :sparkles:[Feat] #S11P31A602-152 #in-progress Prettier 설정 생성
    Former-commit-id: 0a11f325fed7d70d6b8f9303b51d82e7ffa229a1

--- moa/.prettierrc.js
+++ moa/.prettierrc.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:638fd435cf403cd3ac6ce9075a3cc732f90b7c102cb5b78066a7d83987afadde
-size 141
+oid sha256:394f4c3c9e546b3b7d7ebe83d1453e3966b8558c706a2eec7b8931ee06769565
+size 452
