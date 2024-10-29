178ccc879cd3403e1486c5b2bcc1e5fcc87d696f
kimjuhyeong <kkjjhh06@naver.com>
2024-10-29 15:21:49 +0900

    :sparkles:[Feat] S11P31A602-177 JenkinsFile 생성
    Former-commit-id: f92909b450d71267aaf918dae8754f7559b42830

--- backend/src/main/java/com/MOA/backend/domain/group/entity/Group.java
+++ backend/src/main/java/com/MOA/backend/domain/group/entity/Group.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f0a783cf9887ed5cf0f937f0c5e87b929a02d54db9b480d3427e59aad2c386d2
-size 69
+package com.MOA.backend.domain.group.entity;
+
+public class Group {
+}
--- backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
+++ backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6736bc92f66018683f527110332a9660811cccaf7dbfb26240fbbc9dccb2a355
-size 77
+package com.MOA.backend.domain.group.service;
+
+public class GroupService {
+}
