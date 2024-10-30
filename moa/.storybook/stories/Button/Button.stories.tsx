37eff6ae9eabd4ad319285d78241d2712cc6cc44
inception010203 <inception010203@gmail.com>
2024-10-30 09:35:55 +0900

    :sparkles:[Feat] #S11P31A602-148 #in-progress 스토리북 설치
    Former-commit-id: 00eeabf037bd1b25645251ceb928688bbd71169d

---
+++ moa/.storybook/stories/Button/Button.stories.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fb0a9f97f6325aebac71b27e3091364e7242d689494e86ee3ef7bc4294294d13
+size 650
---
+++ moa/.storybook/storybook.requires.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4714dbc532484d13b4a7c02f2377114641280f6a5de170e53c23ca02f87d7456
+size 1218
