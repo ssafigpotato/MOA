365619f1a21ded585822a54be9d0d2379eb350ce
ssafigpotato <ssaf010203@gmail.com>
2024-10-30 15:00:23 +0900

    :bug:[Fix] #S11P31A602-148 #in-progress App.tsx, index.js 기본으로 수정
    Former-commit-id: 341642af520b74b8455b954efdb22dc4fef414b8

--- moa/App.tsx
+++ moa/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:149bc07e76f99266eb987ad5cdaeffd971364e1e543a4b0088e561ee8a6e54ce
-size 2858
+oid sha256:bfaacd53adfa69dd3f8ce4616329b5394d49e6e57a7f4ec96df90561f07e8e10
+size 3504
