05270c1f9eb6ec5f7d47dc40a9eafdbbd14d3bf1
ssafigpotato <ssaf010203@gmail.com>
2024-10-30 14:10:01 +0900

    :sparkles:[Feat] #S11P31A602-148 #in-progress 스토리북 재생성 및 App.tsx,bable, metro config.js 설정
    Former-commit-id: 5da7579c3b87776a870ea595432675685e12a2a1

--- moa/App.tsx
+++ moa/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cde17f07ec860c5241b61380d4f616ef769aa1f344f0a118cc138fee1ace757
-size 2617
+oid sha256:149bc07e76f99266eb987ad5cdaeffd971364e1e543a4b0088e561ee8a6e54ce
+size 2858
