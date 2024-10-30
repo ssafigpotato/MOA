0e67d1f07a13126ade16ec2959ae1b4642d47107
inception010203 <inception010203@gmail.com>
2024-10-31 08:59:48 +0900

    :sparkles:[Feat] #S11P31A602-210 #in-progress 스타일드컴포넌트 설치
    Former-commit-id: 959b316e8a5a5b0f9d63611f42176594c9a315c3

--- moa/babel.config.js
+++ moa/babel.config.js
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6fe1d279658facce638456e650b3b1e9a34d2f5907740d59858309eaf93bdfef
-size 140
+module.exports = {
+  presets: ['module:@react-native/babel-preset'],
+  // babel.config.js
+  plugins: ['react-native-reanimated/plugin'],
+};
--- moa/ios/moa/main.m
+++ moa/ios/moa/main.m
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:75228f2b985839852d2c934250f3cfcfb5acb776c279dbfb4e551491509a9813
-size 199
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
