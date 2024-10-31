09fae23d55f3419769e63fe5f9e9e68172331f01
kimjuhyeong <kkjjhh06@naver.com>
2024-10-31 18:07:36 +0900

    :wrench:[Fix] docker-compose.yml 파일 수정
    Former-commit-id: 9900b49a98a4029aa6c05f1251ac251af7bb8909

--- backend/src/main/java/com/MOA/backend/A602Application.java
+++ backend/src/main/java/com/MOA/backend/A602Application.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:214c10d9ac50feb6581a4782e29f1f818b40f76c32efee7dd8b7321d1a17d4b8
-size 304
+package com.MOA.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A602Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A602Application.class, args);
+	}
+
+}
