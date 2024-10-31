2def5e900a49d51c455bfec077a855bbd8e99905
dla9520 <dla9520@naver.com>
2024-10-31 21:04:22 +0900

    :sparkles:[Feat] #S11p31A602-179 #in-progress App Header 기본 레이아웃 구현
    Former-commit-id: d18e496d324ca14a97ba411067f2efe156227ca5

--- moa/App.tsx
+++ moa/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be929d7fb1200bb7b2872a8ec9aea01f17433d5b9edf7573a66f4cbf2d544296
-size 1099
+oid sha256:c81e2bc90c85e25f829ad8f2fba4514762696d4d0bd3a080750ae2b9c8a13414
+size 1328
