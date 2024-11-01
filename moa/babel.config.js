113ae324ce22509e59a961df8442c3e2be1b2fff
inception010203 <inception010203@gmail.com>
2024-11-01 17:46:39 +0900

    :sparkles:[Feat] #S11P31A602-216 #in-progress env라이브러리 설치, env 생성, babel config 설정, build.gradle 설정
    Former-commit-id: 4608ce564532c744e3b65accccfbfe968fb96b2c

--- moa/babel.config.js
+++ moa/babel.config.js
@@ -1,5 +1,3 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  // babel.config.js
-  plugins: ['react-native-reanimated/plugin'],
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:a437841395a9447b2f8c879c37ff91fc7cdc772c0f3b55715efc8a971c80b9dc
+size 265
