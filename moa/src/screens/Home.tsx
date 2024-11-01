be63fa8ef4d8517dd4d324526d398ec1298c044f
dla9520 <dla9520@naver.com>
2024-11-01 15:17:44 +0900

    :art:[Design] #S11P31A602-166 #in-progress 파일 이름 수정
    Former-commit-id: da5d045f28011d778c622fbc63e99ff836ffdc65

--- moa/src/screens/Home.tsx
+++ moa/src/screens/Home.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ddb2324dc1fe6f76d94ca54a6edfbb1b03bd66671e31f814a9a95f7d122e34c
-size 336
+oid sha256:5dcea4e0719f641c6f0113fa4627c13742c5eb53aa3293cd6012a64bfecd6963
+size 338
