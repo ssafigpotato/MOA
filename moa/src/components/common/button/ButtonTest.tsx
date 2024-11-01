16dd194ad471307aa02b5bffdd502887afb1e654
inception010203 <inception010203@gmail.com>
2024-11-01 14:10:53 +0900

    :wrench:[Remove] #S11P31A602-211 #done 필요없는 파일들 삭제
    Former-commit-id: 5cce693dc0a28651444028b17b4e001fd5eb3847

--- moa/src/components/common/button/ButtonTest.tsx
+++ moa/src/components/common/button/ButtonTest.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b00c8964f47899e83df74ac2bbcb0d8bf8f9717f56c193ad8be7eaa41ac1857
-size 2461
+oid sha256:d1728cf866ebf8c7882684a7ad930ee42c5a9ffb998a2fb78865470d870c2059
+size 2147
--- moa/src/components/common/button/Buttons.styles.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1631e7e3e3fcb21048cd6d35846e3c2961b62032f932a4bd2579474afce6ddba
-size 2581
