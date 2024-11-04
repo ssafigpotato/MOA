e78a4da1403362b6adc35d94abf787d25a699aab
김윤홍 <yunhongkim123@gmail.com>
2024-11-04 18:04:53 +0900

    ✨[Feat] #S11P31A602-244 #done group
    Former-commit-id: 368fc19f6ce21316b7cbceeebcb7aa6180ea8389

--- backend/src/main/java/com/MOA/backend/domain/group/entity/Group.java
+++ backend/src/main/java/com/MOA/backend/domain/group/entity/Group.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0a783cf9887ed5cf0f937f0c5e87b929a02d54db9b480d3427e59aad2c386d2
-size 69
+oid sha256:b1242d1b11ba9c437ef144345f9e57516dbc99c6bf2981130ceaf116e0f9f611
+size 1015
--- backend/src/main/java/com/MOA/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/MOA/backend/domain/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78ffb4754db922fa553a2d69657ea77061a7e9bf3b5b28dad9a619450e462384
-size 510
+oid sha256:b0d237411d2eb253fbf7d7e343a28379b1b7690a9d6ec2e9b1a99144640f0243
+size 672
