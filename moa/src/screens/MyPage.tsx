cd51151f2a9a19c057f721c4148acb44783b5f92
inception010203 <inception010203@gmail.com>
2024-11-04 15:11:56 +0900

    :art:[Design] #S11P31A602-263 #done 마이페이지 구현, 실제 디바이스에서 테스트 필요
    Former-commit-id: d9794d9c29f22765a9b7e181324e88595ab47831

--- moa/src/screens/MyPage.tsx
+++ moa/src/screens/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b3e8a91e21d9831dcbfd367908683b9104429544ce2ba8fdf7d462a3c2980f2
-size 2183
+oid sha256:43f9eeb7a917a8a842ab267fcd05e7b5a442f76cb1bfd0e44863691e1381958a
+size 1934
