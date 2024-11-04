888a27b25b0733c988ece6ae1fbc64bd22138292
김윤홍 <yunhongkim123@gmail.com>
2024-11-04 13:53:48 +0900

    ✨[Feat] #S11P31A602-244 #done login
    Former-commit-id: 7aadef731d8b5530beeb00d0c0fcd76663ff62f5

--- backend/src/main/java/com/MOA/backend/global/auth/jwt/dto/GeneratedToken.java
+++ backend/src/main/java/com/MOA/backend/global/auth/jwt/dto/GeneratedToken.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b67a98658c54cb0020c99c8836f16b7f22236d63b674e8501aaadec5c568092f
-size 229
+package com.MOA.backend.global.auth.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class GeneratedToken {
+    private String accessToken;
+}
