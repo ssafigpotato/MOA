a60c44d4d92ed203cf500ea82aa97375d1fc7d9e
dla9520 <dla9520@naver.com>
2024-11-04 11:04:16 +0900

    :art:[Design] #S11P31A602-166 #done 나의 그룹 조회 UI 구현 완료
    Former-commit-id: 1dd89c03ff3515087f9d5c47a55de380671f7687

--- moa/src/components/group/MyGroupListItem.tsx
+++ moa/src/components/group/MyGroupListItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4646387f63eebf5fe5dcd6fc17a2764f8ffcab77c0ebd05312525ead62190c1
-size 1751
+oid sha256:37da85076f4ad425c564c5d3b79260aecb53a927671c5b61d9221253c2df7c02
+size 2252
