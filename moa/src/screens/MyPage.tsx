1372de50909481d63c8c7bc721a4899e720f9a78
inception010203 <inception010203@gmail.com>
2024-11-04 13:21:39 +0900

    :art:[Design] #S11P31A602-263 #in-progress 사용자 닉네임 색상 maindarkorange로 수정
    Former-commit-id: 6ad80da290131bad35c60eb02cd0fdca2a712ae8

--- moa/src/screens/MyPage.tsx
+++ moa/src/screens/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d85bb53eda0c8de49fa65f083476e960f872fb1d0b1aff45e58b2fcba36edb5
-size 2959
+oid sha256:9fb705e9144d6a98c07030164e38a91f4ec6ebc83781b008a80a0dfcae3cfdc6
+size 3826
