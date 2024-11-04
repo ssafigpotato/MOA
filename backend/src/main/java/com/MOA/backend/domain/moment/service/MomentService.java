359c119ac4769865a2e025afe117946e8756a493
kimjuhyeong <kkjjhh06@naver.com>
2024-11-04 19:23:53 +0900

    :sparkles:[Feat] #S11P31A602-270 순간 삭제 API 구현
    Former-commit-id: 2187612335ae9dfa1bc4692dc5cf6791f7e971fa

--- backend/src/main/java/com/MOA/backend/domain/moment/service/MomentService.java
+++ backend/src/main/java/com/MOA/backend/domain/moment/service/MomentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e35f4eaa8946f9d72a08811bbacb92082c664e28ecf148b0c97d866ac68c5b6
-size 2529
+oid sha256:ff10141b5aa03d70a976de34364c3e454e86e261e8ebfaaba552e3a61faa257e
+size 2682
