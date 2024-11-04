02ff4a27e55d60f8ab19e28982460fafd457b154
dla9520 <dla9520@naver.com>
2024-11-04 15:16:50 +0900

    :art:[Design] #S11P31A602-230 #in-progress 모아 생성 UI 구현
    Former-commit-id: c96da3ab22d752f3f7ec5f3ab2e12315ae43285b

--- moa/src/components/common/ScreenContainer.tsx
+++ moa/src/components/common/ScreenContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21be861045916fc2a6bdad02bc3d237a9d0e96d52d922978e49b74466c35c2f6
-size 427
+oid sha256:cd572022e56b4a2c312b3f3a67369a97a284d74ff7b03b09c02c94e0e25c6cdd
+size 437
