6a7a3dfc74bed98adf75ac7c3ee436fe99a7e0be
inception010203 <inception010203@gmail.com>
2024-11-04 13:57:31 +0900

    :sparkles:[Feat] #S11P31A602-263 #in-progress 얼굴 등록 컴포넌트 및 사진 찍기, 사진 라이브러리 접근 기능 구현
    Former-commit-id: 4586da1a7929f9840672ed58d90cb1396abb1b6e

---
+++ moa/src/components/mypage/FaceImage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5076fe40efaceedb9c1e89af2aea958b229a30f7739e46b6b6da3dc30b6aa469
+size 3112
--- moa/src/screens/MyPage.tsx
+++ moa/src/screens/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbb029ad9420099490db1b6d6abdd75ac34275682e097ebbdea806ae1e97eeaa
-size 2338
+oid sha256:62719bdc0bfdda6b32c4ed76a9ebed79483bc6d25186b192fa0b240b53f64345
+size 2175
