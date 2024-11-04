2862821b41f9d14b7ae9ba6f526c286424acb62c
inception010203 <inception010203@gmail.com>
2024-11-04 13:38:36 +0900

    :recycle:[Refactor] #S11P31A602-263 #in-progress 유저 정보 컴포넌트 분리
    Former-commit-id: 5daaf5335eaa5bb21fe0ea374a7cfa9e3fc1d669

---
+++ moa/src/components/mypage/MyInfo.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:96b5beff9496a95c0188a95e2825a76889b9d5dd11786cd4ce2a0b99207df589
+size 3095
--- moa/src/screens/MyPage.tsx
+++ moa/src/screens/MyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fb705e9144d6a98c07030164e38a91f4ec6ebc83781b008a80a0dfcae3cfdc6
-size 3826
+oid sha256:fbb029ad9420099490db1b6d6abdd75ac34275682e097ebbdea806ae1e97eeaa
+size 2338
