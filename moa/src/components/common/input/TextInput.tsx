5ff9578796a7e9490b986cd585546303a14b0ff7
dla9520 <dla9520@naver.com>
2024-11-04 20:12:26 +0900

    :art:[Design] #S11P31A602-230 #done 그룹 생성 UI 구현 완료
    Former-commit-id: 9c84e6dd962e2c0f2e54ac53eb61d389b9beec41

--- moa/src/components/common/input/TextInput.tsx
+++ moa/src/components/common/input/TextInput.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b2c6f592a5f5851161ca077bd5906f2f351aedc62634514c891a3d69c601d7a
-size 334
+oid sha256:b7f14594ba1ccbc43b460efff7b31a0bdd2d1596566b0987be7f1d3c2376b4de
+size 633
