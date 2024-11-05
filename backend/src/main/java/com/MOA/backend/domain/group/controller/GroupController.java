21742ef5991d4eff0e64c9a628f0407d3e78c704
김윤홍 <yunhongkim123@gmail.com>
2024-11-05 14:15:19 +0900

    ✨[Feat] #S11P31A602-244 #in-progress swagger-config
    Former-commit-id: a9d0a6622574bb4ad81bafcd78d87b94c2a3e473

--- backend/src/main/java/com/MOA/backend/domain/group/controller/GroupController.java
+++ backend/src/main/java/com/MOA/backend/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98d06d36004c9f7db02efa5181a6213dcda1c06ee4a61d6fb7876c63ee9a6535
-size 2874
+oid sha256:a5aa5fa4fddcfeb5cea1295d2fa1c947279b4cde80d739182d8b91c37f0c7e12
+size 4250
