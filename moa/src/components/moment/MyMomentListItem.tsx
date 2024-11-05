3778ff499734ffc9832dd1c43dc7c2186cee4455
dla9520 <dla9520@naver.com>
2024-11-05 13:21:51 +0900

    :sparkles[Feat] #S11P31A602-167 순간 타입 설정
    Former-commit-id: f43e80900f449bcb67ac6d4ce72896a32f379c75

---
+++ moa/src/components/moment/MyMomentListItem.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f691abdcfab97def5cfd13f3ee1691c40e165c449c908a72010703ce077ed57
+size 214
---
+++ moa/src/types/moment.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc02d28d155f334d194cba93d2c0f11fcfc2e83497bf2bfd9e8069116204e057
+size 146
