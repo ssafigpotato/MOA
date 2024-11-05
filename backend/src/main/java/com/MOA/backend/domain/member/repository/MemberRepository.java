c710675eca3277b70fdde2e4dc25432837c500cf
kimjuhyeong <kkjjhh06@naver.com>
2024-11-05 09:50:35 +0900

    :ambulance:[!HOTFIX] Jenkins 빌드 오류 수정
    Former-commit-id: 15399212520393fdda654c8df09bdeb9b2634274

--- backend/src/main/java/com/MOA/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/MOA/backend/domain/member/repository/MemberRepository.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:792bff670c3ae50a175dd116373d4f34de0bcb6654c3125315390ecb41d3924f
-size 85
+package com.MOA.backend.domain.member.repository;
+
+public class MemberRepository {
+}
