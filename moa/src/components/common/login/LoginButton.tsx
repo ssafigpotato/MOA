12f74dbe0ac1343856e7fd59ebf17261e1e07398
inception010203 <inception010203@gmail.com>
2024-11-05 10:58:03 +0900

    :sparkles:[Feat] #S11P31A602-155 #in-progress 로그인 버튼 기능 구현
    Former-commit-id: 13ea7f29365d01f73bdcd3a51b43c4ac2e5bad48

--- moa/src/components/common/login/LoginButton.tsx
+++ moa/src/components/common/login/LoginButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df4f15c8565e4b7b6db8736e57fe2fe1e544c05984d860a98b71dcb1dc693d3e
-size 113
+oid sha256:7af4187cf645adf43b506a26e8cdc3c52445cd395dbfc9653d4dd9a9ead5625a
+size 887
