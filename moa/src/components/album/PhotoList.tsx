f2a224cef0f510bc3d4a6655db98163c93301e34
dla9520 <dla9520@naver.com>
2024-11-07 19:28:56 +0900

    :sparkles:[Feat] #S11P31A602-231 #done 그룹 사진 다운로드 기능 구현
    Former-commit-id: 802c8641fbd1c84e12b39eac1c047f7e1a25a27d

--- moa/src/components/album/PhotoList.tsx
+++ moa/src/components/album/PhotoList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a00b8059a267ca15ad3b71ad0186a6ccbbfffe69bc9c48fca3a3ca0fa71951e
-size 3206
+oid sha256:ca0c4bcc3aae27d75852df7ed66e2d5ad518c15e74ce2ec6a395a60b4690c93b
+size 3520
--- moa/src/components/group/groupDetail/GroupDetailHeader.tsx
+++ moa/src/components/group/groupDetail/GroupDetailHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e27877f54a3ee6e9d2c7032c3ee09fd40fd70e2f9dff32ee582a4da01fa0152
-size 4920
+oid sha256:87da5c692b674c49cddfe77c804282182b6614d9fc4e37720b5c3d22d4a23faf
+size 4953
