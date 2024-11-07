7276a34fbd637403462a44f9f4ea312c8f0b0c2d
kimjuhyeong <kkjjhh06@naver.com>
2024-11-07 14:55:30 +0900

    :wrench:[Fix] 사진 파일 확장자 jfif 다시 추가
    Former-commit-id: 7ec87768a1e269a7a14696abf0802257a7c267f4

--- backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
+++ backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5b2c62b8012c19f9c8526277f6496c5423ec6ea98ce35c4957b9b45149059c3
-size 6942
+oid sha256:4f798b8ba168d9b4b6b254b4a81bc9ad23771b52ea04fef6def6dfca32d58ca1
+size 6951
