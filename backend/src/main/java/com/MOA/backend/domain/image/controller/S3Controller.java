f48581c4ba821d6dd25e3227adc4c58603e4108b
kimjuhyeong <kkjjhh06@naver.com>
2024-11-07 14:35:16 +0900

    :sparkles:[Feat] #S11P31A602-275 #in-progress 유저 프로필 업로드 API 생성
    Former-commit-id: d2b7d0b9faaf96092b36b764c4cf478cbf0a74d1

--- backend/src/main/java/com/MOA/backend/domain/image/controller/S3Controller.java
+++ backend/src/main/java/com/MOA/backend/domain/image/controller/S3Controller.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2bc6a9cd5a873454f8af1a4ef77f99344e03aeae5e70475e96312696454942d5
-size 1126
+oid sha256:e29ecdd2873a1dae661521b7ecc6de9cfa88a4ed9baa6166aa496d10acee1a28
+size 1211
