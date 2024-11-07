d5c94f1234d67eca39a5af993fa5331d15331dd1
dla9520 <dla9520@naver.com>
2024-11-07 13:55:19 +0900

    :art:[Design] #S11P31A602-231 #in-progress 사진 옵션 및 다운로드 UI 구현
    Former-commit-id: 3ee0447437f1d999ac27962f35be534ddcc28899

--- moa/src/components/group/groupDetail/GroupDetailHeader.tsx
+++ moa/src/components/group/groupDetail/GroupDetailHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5e1467648e7530359b575a291f216a8403c3af861a4b03cf85d723a6e9b70d2
-size 4872
+oid sha256:2e27877f54a3ee6e9d2c7032c3ee09fd40fd70e2f9dff32ee582a4da01fa0152
+size 4920
--- moa/src/screens/group/GroupDetail.tsx
+++ moa/src/screens/group/GroupDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e08a51321358e31a27d0233ed6d50073a2c1f55daba1214e1327fa7e7c191ef
-size 2046
+oid sha256:670f12c9029fd529c45bea974ead045386f8e2463217ba5c3d07517a6177fcf7
+size 2122
