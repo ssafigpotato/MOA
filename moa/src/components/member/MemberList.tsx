56429705a2dfe4329b7109138630d9c5ae2138aa
dla9520 <dla9520@naver.com>
2024-11-07 15:34:17 +0900

    :art:[Design] #S11P31A602-231 #in-progress Photo List UI 구현
    Former-commit-id: e4403a96633bd1db59bd6a326d6860eb86b79c24

--- moa/src/components/member/MemberList.tsx
+++ moa/src/components/member/MemberList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85dadb6d24dbe68ea48f82d9621eecfcea249423454823d695a12981c2fd1e17
-size 1818
+oid sha256:e24d2df182a3af4a01c0ccc17b433c73730275c2aa500157f9ef00ec1e83b1a4
+size 1791
