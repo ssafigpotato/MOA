2494ed5728ebbe71c63a5ee3a98fa6cbe212d5e9
dla9520 <dla9520@naver.com>
2024-11-07 10:56:49 +0900

    :art:[Design] #S11P31A602-231 #in-progress screen partition 구현 완료
    Former-commit-id: e6377d44203f371a20be72640c2f4df546ca2e58

--- moa/react-native.config.js
+++ moa/react-native.config.js
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0db3304d088f72fc936735c288523692345500e486f52be5cd976d0db3b4ba88
-size 120
+module.exports = {
+  assets: [
+    './src/assets/fonts/',
+    './node_modules/react-native-vector-icons/Fonts',
+  ],
+};
