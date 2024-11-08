4faaf2403dc50ae5ee27611e89a6b229106ab3b5
inception010203 <inception010203@gmail.com>
2024-11-08 15:12:44 +0900

    :sparkles:[Feat] #S11P31A602-156 #in-progress zustand로 authStore생성 및 로그인 로직 구현
    Former-commit-id: 42270ec70aec5a08c0fbb8f75ca09adcdee7cffa

--- moa/src/components/common/login/LoginButton.tsx
+++ moa/src/components/common/login/LoginButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31df5e501cc218be1c17fdabca94241f40ccc2d9f922e8c84b72f7e694436d59
-size 3112
+oid sha256:f7da680048b54c7528671df1148a069d39a2c4a0b03a7e76c130156dbf62bb18
+size 4700
---
+++ moa/src/stores/authStores.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac644e4a179dcb95a72a6669962cc086e407b0cc939b011d0feb7b614c52ae12
+size 1640
