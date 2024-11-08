454ab7cebe489cb1bde2c2b033f581e92ddfb132
kimjuhyeong <kkjjhh06@naver.com>
2024-11-08 21:21:49 +0900

    :sparkles:[Fix][Feat] Member 닉네임 추가 및 닉네임 관련 로직 변경
    Former-commit-id: b1dc7acc97a2875115329b1c499ca3086bff9b73

--- backend/src/main/java/com/MOA/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/MOA/backend/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24da5d3e827da8084116b064a2bd5bdb0d8136dccc707f6ff566bc26ef7992b7
-size 279
+oid sha256:42cf305cbc32d6b8ab0742d1210cc0f5cc5df9128960c5ce5e3fd3815ee75dcc
+size 834
--- backend/src/main/java/com/MOA/backend/domain/member/service/MemberService.java
+++ backend/src/main/java/com/MOA/backend/domain/member/service/MemberService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9caf49bcd53b05c512a476f96b09b2d0aa1c0d053350690dce18e45688380a2
-size 985
+oid sha256:7c7456334732e53c130a0ce17423b0fee53fa8374dc33ee0bf460093f0da934a
+size 1411
