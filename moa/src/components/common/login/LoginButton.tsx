ec3989dc76135b530e8ec1f201646e65f5ee495b
inception010203 <inception010203@gmail.com>
2024-11-08 16:04:46 +0900

    :sparkles:[Feat] #S11P31A602-156 #in-progress 로그인 관련 api.ts 수정
    Former-commit-id: 65bdbf1092a998140a58cdbdc9ee1d00c850dba2

--- moa/src/components/common/login/LoginButton.tsx
+++ moa/src/components/common/login/LoginButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7da680048b54c7528671df1148a069d39a2c4a0b03a7e76c130156dbf62bb18
-size 4700
+oid sha256:fb341029b93aa87de26db9ee539f7b280d541523f0e87e6b722f5dc084ba66ee
+size 4760
