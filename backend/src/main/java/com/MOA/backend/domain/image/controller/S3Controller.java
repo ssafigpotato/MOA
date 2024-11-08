78a8ac567d00657ae770f93ba74957dfbc3784c0
kimjuhyeong <kkjjhh06@naver.com>
2024-11-08 21:30:10 +0900

    :sparkles:[Fix] 그룹 내 자동 순간 생성 Param 수정
    Former-commit-id: 0b6d7dd6d6519b8af33a13d88a4c9eea64303fd2

--- backend/src/main/java/com/MOA/backend/domain/image/controller/S3Controller.java
+++ backend/src/main/java/com/MOA/backend/domain/image/controller/S3Controller.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdf8f9079145d486325a4f7e61332ed74c623295ff8eb5197e06c460549af719
-size 2162
+oid sha256:83625f75b796f14de0f0ccaeb7d0ab69ee49e1415cc5074824931ba3b5d65d8e
+size 2167
