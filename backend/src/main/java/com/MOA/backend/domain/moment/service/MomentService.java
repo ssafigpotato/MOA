4f36295c6546bbd1a526737e497fcabbc21f978e
kimjuhyeong <kkjjhh06@naver.com>
2024-11-08 21:30:10 +0900

    :sparkles:[Fix] 그룹 내 자동 순간 생성 Param 수정
    Former-commit-id: 0b6d7dd6d6519b8af33a13d88a4c9eea64303fd2

--- backend/src/main/java/com/MOA/backend/domain/moment/service/MomentService.java
+++ backend/src/main/java/com/MOA/backend/domain/moment/service/MomentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac9ba9eebe28a52e025bad616fd38f3d71854620eaa4673c948ff233e853bdf8
-size 10809
+oid sha256:f79b81d17101f78b3360f49261b59caef955a844401a376fb0a1e4634fe4b4ef
+size 10540
