8053a75a225dcedf4b541f2faf00b320f0173e5b
dla9520 <dla9520@naver.com>
2024-11-08 14:46:46 +0900

    :art:[Design] #S11P31A602-288 #in-progress css 수정
    Former-commit-id: ec7f4f8f1b78f5e01dc74ed6a0a14e26d0cbccc5

--- moa/src/components/moment/momentDetail/MomentDetailHeader.tsx
+++ moa/src/components/moment/momentDetail/MomentDetailHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc02915af78aad88a701c834b66a84034e7f8498db55758bfba23b56260f420c
-size 4440
+oid sha256:d1e7cda8834652d8c4f96ab61366daa316c8314d7eb7605b38658c674e1fc01b
+size 4442
