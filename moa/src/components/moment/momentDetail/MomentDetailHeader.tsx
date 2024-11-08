ea88d8b9b42343b8320287b1ade919432cc7df23
dla9520 <dla9520@naver.com>
2024-11-08 14:00:04 +0900

    :art:[Design] #S11P31A602-288 #in-progress 순간 상세 스크린 헤더 UI 구현
    Former-commit-id: d31d25874b405605acf71a3e0f645b1f71a3b77d

--- moa/src/components/moment/momentDetail/MomentDetailHeader.tsx
+++ moa/src/components/moment/momentDetail/MomentDetailHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b3ed58e5f6f4697b2559691d09861d5b51a055f2214487f9c44f886016d0ff6
-size 396
+oid sha256:dc02915af78aad88a701c834b66a84034e7f8498db55758bfba23b56260f420c
+size 4440
