1eda9e59e5dc752b4bbebce3fd73ad0b131dabe0
kimjuhyeong <kkjjhh06@naver.com>
2024-11-08 19:10:28 +0900

    :sparkles:[Feat] Member 닉네임 필드 추가 및 닉네임 수정 API 생성
    Former-commit-id: 2f0f615ad6650750a76c9b7fd3a936b8c6a05805

---
+++ backend/src/main/java/com/MOA/backend/domain/member/dto/response/MemberResponseDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7b4ab0b08af47393c74bd9d61c23d5ba26c8171ec842bb6f37cc047e23528816
+size 150
--- backend/src/main/java/com/MOA/backend/domain/member/entity/Member.java
+++ backend/src/main/java/com/MOA/backend/domain/member/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:513b289355be0c931b4d95d7048a707307baf2f3caf6ab7950de8f89b2d21227
-size 983
+oid sha256:cd4ed90b2a3d5a9986c67e0621e3c4d4fc65ae4499f15ed8a76037469197a86b
+size 1096
