2057d2940d55204710658a91af2cc3ee3ecb9120
min_yl8234 <min_yl8234@daum.net>
2024-11-08 16:13:50 +0900

    :sparkles:[Feat] #S11P31A602-253 얼굴 등록 API 작성
    Former-commit-id: 1975f7309804af366d83d2277b12a6d4c0fa5384

--- backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
+++ backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85a36360c56d474942a67cd2c2225a6bb08720d2b0244870e7f6a90033595831
-size 9485
+oid sha256:deb8c8f4a81477ed27bf7d5fa5dd3f3907dc2ff4bd3e7c70d0066f1bdcaff49f
+size 10097
---
+++ backend/src/main/java/com/MOA/backend/domain/image/util/RegistFaceUtil.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:73aba3476a0e9824788f9fd91d0e430b2587c7881bbb230a9877f32ee0e07c5b
+size 1709
