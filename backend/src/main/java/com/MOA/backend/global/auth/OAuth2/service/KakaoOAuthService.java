31b977e1706f4af522797d353271ab4e539cfbf4
김윤홍 <yunhongkim123@gmail.com>
2024-11-09 22:31:01 +0900

    :ambulance: [Fix] #S11P31A602-245 카카오 OAuth
    Former-commit-id: 1f76a612ef8d4eee149653b4a3680f73d003d1b0

--- backend/src/main/java/com/MOA/backend/global/auth/OAuth2/service/KakaoOAuthService.java
+++ backend/src/main/java/com/MOA/backend/global/auth/OAuth2/service/KakaoOAuthService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94c802a628dad49a244df4ff032b9adb848da590dc0db348a80bae7d9ad8cdc0
-size 2689
+oid sha256:12ee8f9c1ca256c83f14d4de9fba5b950f1a2c8fbe61a94a380d0585d9b93e70
+size 3260
