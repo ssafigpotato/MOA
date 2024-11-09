dea0a84cbe288e1242722c40a618daf811f8752e
inception010203 <inception010203@gmail.com>
2024-11-09 19:21:59 +0900

    :sparkles:[Feat] #S11P31A602-291 #in-progress 카카오 공유하기 설정, KakaoActivity 생성
    Former-commit-id: 91b522e42b241285db1847cc6fbec9b89baaef43

--- moa/src/components/moment/momentDetail/MomentDetailHeader.tsx
+++ moa/src/components/moment/momentDetail/MomentDetailHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1e7cda8834652d8c4f96ab61366daa316c8314d7eb7605b38658c674e1fc01b
-size 4442
+oid sha256:6ab94fd1484ea4f495cb20a5984842d91598ea9e9a9b7762443e6cb95c53cb29
+size 4749
