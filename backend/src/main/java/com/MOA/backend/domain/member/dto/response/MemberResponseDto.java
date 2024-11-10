7b89bb2c2bfe9d1f8faf2f437a86cd89ca7a8050
kimjuhyeong <kkjjhh06@naver.com>
2024-11-11 00:33:02 +0900

    :wrench:[Fix] 트랜잭션 처리를 위핸 MongoCluster 생성
    Former-commit-id: 8fffbbc8393c38a47585bd6809cacacb7cbe7a4a

--- backend/src/main/java/com/MOA/backend/domain/member/dto/response/MemberResponseDto.java
+++ backend/src/main/java/com/MOA/backend/domain/member/dto/response/MemberResponseDto.java
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7b4ab0b08af47393c74bd9d61c23d5ba26c8171ec842bb6f37cc047e23528816
-size 150
+package com.MOA.backend.domain.member.dto.response;
+
+import lombok.Builder;
+
+@Builder
+public class MemberResponseDto {
+    private String nickname;
+}
