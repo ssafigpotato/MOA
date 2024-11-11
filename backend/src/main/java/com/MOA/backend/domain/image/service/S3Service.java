52df95ac7bd9c41a778873f3307e41f5281786c0
kimjuhyeong <kkjjhh06@naver.com>
2024-11-11 16:38:01 +0900

    :wrench:[Fix] S3 Service 수정
    Former-commit-id: eba3c0b8837354f7635e7864262f932681d8f8db

--- backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
+++ backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d9b45650893ac0088cbac459f78ac745a6c1d9acc22179edbd1aa06d1b850ad
-size 15145
+oid sha256:381d0fa9995d9e8de2342bd248cdf8db325beae498934a28a87242340c57b1ba
+size 14862
