548440d2da734b69d6b531b8c6ce033d045c32f6
김윤홍 <yunhongkim123@gmail.com>
2024-11-11 17:27:59 +0900

    :ambulance: [Fix] S11P31A602-298 Moment api수정
    Former-commit-id: d69737949724e65b9f0a024b7bd5d361fca9f043

--- backend/src/main/java/com/MOA/backend/domain/moment/entity/Moment.java
+++ backend/src/main/java/com/MOA/backend/domain/moment/entity/Moment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6b402b7f873af32df8aac09f111ab000107154d62090482209ebe33716b646e
-size 1815
+oid sha256:748cb934fdb985fc484d7167561557675a268a94bb46cd31a909c9cc1979adc5
+size 1927
