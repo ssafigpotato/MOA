ee71110143e1aaa9834e768a4e423fc0989891b3
kimjuhyeong <kkjjhh06@naver.com>
2024-11-11 16:24:14 +0900

    :wrench:[Fix] 순간 상세 조회 이미지 URL DTO 추가
    Former-commit-id: 6723498752d30acc49ec9c02f861289c7a0351ad

--- backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
+++ backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf5620bd98e673c7a976f10ebc5a7fdb251b7c8827723bf03c3288aa0202426a
-size 11909
+oid sha256:9d9b45650893ac0088cbac459f78ac745a6c1d9acc22179edbd1aa06d1b850ad
+size 15145
