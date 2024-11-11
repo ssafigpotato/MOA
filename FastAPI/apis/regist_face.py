05b3410471b635582c6d7cd2cb49f3da2d066a38
root <root@ip-172-31-40-120.ap-northeast-2.compute.internal>
2024-11-11 11:48:36 +0000

    :sparkles:[Feat] #S11P31A602-252 FastAPI 서버 띄우기
    Former-commit-id: 3a4089a8d65678cdb0fe92fe9cf7c1ca2cfea1ac

--- FastAPI/apis/regist_face.py
+++ FastAPI/apis/regist_face.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:988f8af6dffaa60d3be23fa0be0d5d0e9359e7b8d8b3a67cea85c4328e82ae59
-size 2245
+oid sha256:2bc033e17656e9a525a8147dedcc1ffd80a396d2a6fbc09cbdc939584dc5d1e2
+size 2246
