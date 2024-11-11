3ad17150a0fe3f77030b72b5e1f428e6d08fd5e1
kimjuhyeong <kkjjhh06@naver.com>
2024-11-11 16:24:14 +0900

    :wrench:[Fix] 순간 상세 조회 이미지 URL DTO 추가
    Former-commit-id: 6723498752d30acc49ec9c02f861289c7a0351ad

--- backend/src/main/java/com/MOA/backend/domain/image/dto/FaceEmbeddingDTO.java
+++ backend/src/main/java/com/MOA/backend/domain/image/dto/FaceEmbeddingDTO.java
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2df59027b4764a262a0f44c92281604d16a4b6b3c616dd03fb5e258c30549163
-size 199
+package com.MOA.backend.domain.image.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class FaceEmbeddingDTO {
+    private Long userId;
+    private byte[] faceEmbedding;
+}
