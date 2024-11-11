5c8366a42ecfc654a79019da0b91aacd869be1f8
김윤홍 <yunhongkim123@gmail.com>
2024-11-12 08:58:18 +0900

    :ambulance:[Fix] S11P31A602-298 jwt토큰 만료 수정
    Former-commit-id: 3e66c4331ea27807c9e683c36dac56f30a556334

--- backend/src/main/java/com/MOA/backend/global/auth/jwt/service/JwtUtil.java
+++ backend/src/main/java/com/MOA/backend/global/auth/jwt/service/JwtUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8c08d52690f11be0c9e9ae85af5fb1316fae7c3db4205af0d0f73eb4e17af87
+oid sha256:24c22e64bbaaa9dc0973d5b9a3a198843e3f9d1e60c801bfb8696a7d9a2c3d29
 size 5067
