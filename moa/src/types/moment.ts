9cbd1f3d0f53d3fbd2cdd299b06f06665669a198
dla9520 <dla9520@naver.com>
2024-11-11 09:50:35 +0900

    :sparkles:[Feat] 나의 순간 생성 API 구현
    Former-commit-id: cb2504f640cad24776f09dc6ff21bd684d7ddc00

--- moa/src/types/moment.ts
+++ moa/src/types/moment.ts
@@ -1,14 +1,3 @@
-export type MomentInfo = {
-  momentId: string;
-  momentName: string;
-  momentOwner: string;
-  createdAt: string;
-};
-
-export type MomentInfoDetail = {
-  momentId: string;
-  momentName: string;
-  momentOwner: string;
-  momentDescription: string;
-  createdAt: string;
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:176b60f5b599ac6d31e8d4012e92f6dfd9366d08edbc5374ce33456fc0d6a779
+size 361
