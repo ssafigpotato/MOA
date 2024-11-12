cf26bd81418a762e9195ef40ead57fb68fbc68c7
inception010203 <inception010203@gmail.com>
2024-11-12 14:39:32 +0900

    :sparkles:[Feat] #S11P31A602-339 #in-progress user정보, userGroup 정보 get api 연결
    Former-commit-id: c95564bd50725203643ba10219a01a60438620b4

--- moa/src/components/mypage/MyInfo.tsx
+++ moa/src/components/mypage/MyInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68783b4340e969e0bf577c4f528af08686fb90c7b7c6583b9d7b45aa8d266a79
-size 2980
+oid sha256:f0586d6bc065d3dea53da54a6bb6dae22f796f71c15ed2f17ff9514c09086fff
+size 3330
