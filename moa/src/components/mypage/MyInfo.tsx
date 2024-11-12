a958564a2d9a48c96cf2736bea9f7729470f2744
inception010203 <inception010203@gmail.com>
2024-11-12 16:40:06 +0900

    :sparkles:[Feat] #S11P31A602-339 #in-progress 유저의 닉네임, 프로필 사진 수정 api 연결
    Former-commit-id: f32b8077948c051e76c01deb1dd1fa34220d9732

--- moa/src/components/mypage/MyInfo.tsx
+++ moa/src/components/mypage/MyInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:251086ff7ceb77c08591f6745ec8d5aa070b7eea95df9f13a2f85ca8722da19a
-size 4900
+oid sha256:5566af720f1d9a3df4caaf09161b8b25d1b35c3aeee6e32f5e7b219b1b05910a
+size 5426
