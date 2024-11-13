23705810363413f3013019b8706f4ba5343d6a47
kimjuhyeong <kkjjhh06@naver.com>
2024-11-13 15:06:26 +0900

    :sparkles:[Feat] Spring Batch 사진 삭제 및 Document 삭제 청크 방식 구현
    Former-commit-id: 5bd63c15a38cad572be5e363f8db4c4f53c44b41

--- backend/src/main/java/com/MOA/backend/global/batch/scheduler/BatchSchedulerImpl.java
+++ backend/src/main/java/com/MOA/backend/global/batch/scheduler/BatchSchedulerImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c68c8805bad8c37747753e35e029c27f176b89a87df97902b495597ad0bcd63
-size 1591
+oid sha256:fdc8435c541849db41d65557c5438b9d85258e5e57fa62216bae0ff02097bbc0
+size 1948
