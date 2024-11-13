54e5077c35683eb1f6c21eceefdaa991e20d0459
kimjuhyeong <kkjjhh06@naver.com>
2024-11-13 20:36:05 +0900

    MERGE
    Former-commit-id: 97cfdc698dceebbad419d583c8f13fe425bd7f81

--- backend/src/main/java/com/MOA/backend/domain/group/controller/GroupController.java
+++ backend/src/main/java/com/MOA/backend/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11bec989dad1b95cbd9b3c60c89dd46426c094d8a59e843d20caca0d8b2feae5
-size 6088
+oid sha256:276177dc81914895a4b7d7bc17fd161d6542daae6612c29d144d2f97254c337e
+size 5973
