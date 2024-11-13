0e8f3f34420c595e7a47f66d5f3c0a6a02168ad8
kimjuhyeong <kkjjhh06@naver.com>
2024-11-13 14:28:25 +0900

    :wrench:[Fix] 불필요한 로그 및 주석 수정
    Former-commit-id: 2c1dbaa74bdf2595d951308614d26ff5378074f2

--- backend/src/main/java/com/MOA/backend/domain/moment/entity/Moment.java
+++ backend/src/main/java/com/MOA/backend/domain/moment/entity/Moment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cab489087a4d144a3ea5462ae71c7b40cd152260b2504b3881796892302f51b
-size 1998
+oid sha256:6b53ba0d50a7a2e886a8fc75e257e2336c52b7ccb27b6f42ba95094f1775c131
+size 2030
