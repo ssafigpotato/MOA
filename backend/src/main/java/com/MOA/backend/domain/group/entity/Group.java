dd137e7a8666f204da4d2ce6be8af92794603169
김윤홍 <yunhongkim123@gmail.com>
2024-11-13 19:51:55 +0900

    :sparkles: [Feat] S11P31A602-296 fcm 연걸
    Former-commit-id: fe243b992a7b59585bad919af9e72a0d81951a98

--- backend/src/main/java/com/MOA/backend/domain/group/entity/Group.java
+++ backend/src/main/java/com/MOA/backend/domain/group/entity/Group.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c0c9559b4fcd8a5b79f7ab317f116bb209308398f309782f56075bcd8a2daf6
-size 1247
+oid sha256:6dbfef56fd9c63ade05b0939eada1a5bc4bf3eef4499ecde5bf289ce5f1f10b1
+size 1223
