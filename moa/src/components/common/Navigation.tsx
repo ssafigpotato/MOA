51fa1ab2863642b79f766fc991df4a647976f322
dla9520 <dla9520@naver.com>
2024-11-13 18:54:57 +0900

    :art:[Design] #S11P31A602-304 #in-progress 사진 상세보기 gesture-handler 추가
    Former-commit-id: c3a7034787b4b0c8c3efe507fa22470426c903cc

--- moa/src/components/common/Navigation.tsx
+++ moa/src/components/common/Navigation.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c93c071d08b002a7f279491fc7d2c19d4f92cbda75f49c789954e4f06b831a66
-size 6525
+oid sha256:c27c824c11cd68ac1b6f39a2695bc81dcdfc6cfd1572c80e861bc22361870622
+size 6717
