a756306f4c885c4a31e5f652177d7e6fcb2b8e24
min_yl8234 <min_yl8234@daum.net>
2024-11-13 17:33:48 +0900

    :sparkles:[Feat] #S11P31A602-326 얼굴 비교 및 분류 API 완료
    Former-commit-id: df270bbbd58acbf81d64b419e9612da8cfe93234

--- backend/src/main/java/com/MOA/backend/domain/group/entity/Group.java
+++ backend/src/main/java/com/MOA/backend/domain/group/entity/Group.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:156974eb85458bac1be394979b6a9d19e544a3f28944734ee682a2ef8cf24266
-size 1285
+oid sha256:ce47eeea453273d676d4ecd1172b1ca6d584caf4e4e78e2123f94f615da5fe6b
+size 1254
--- backend/src/main/java/com/MOA/backend/domain/image/util/CompareFaceUtil.java
+++ backend/src/main/java/com/MOA/backend/domain/image/util/CompareFaceUtil.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7258c596e5c605d72d5cd429be489198ea36a2b9c3a342dfcb0f68713f42ee73
-size 3640
+oid sha256:4faeca7e8e27887bd18e0771cf11a7226306e048b536ddfcad17c822c7d50dcc
+size 3172
