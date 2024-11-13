58657be1a089df215c593f00f2075f1b27f200ef
kimjuhyeong <kkjjhh06@naver.com>
2024-11-13 17:19:49 +0900

    :ambulance:[!HOTFIX] 유저 프로필 저장 S3 경로 수정 핫픽스
    Former-commit-id: c345c3b1de906d0b99da68596045b3348d7ed85c

--- backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
+++ backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aba820b6dc1392fd1338a80d2f1eb3f2c01c2914bfb697f7652949ae9aa3edd
-size 14187
+oid sha256:5313b7966b9f304a2236b556f586ff842b9ff23a165babe0756ebfa38e521186
+size 14186
