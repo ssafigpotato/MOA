8d398b826db8cda298aca257990d4628e2da2381
kimjuhyeong <kkjjhh06@naver.com>
2024-11-13 10:21:55 +0900

    :wrench:[Feat] 스프링 배치를 위한 DeletedMoment 객체 생성
    Former-commit-id: 38ba0730937c5166d91d550901b170f9bd66c6c7

---
+++ backend/src/main/java/com/MOA/backend/domain/moment/repository/DeletedMomentRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9e8126ffe56a797f2719622eca9dee5aec441456cce781d10e749430b91e31db
+size 612
--- backend/src/main/java/com/MOA/backend/domain/moment/service/MomentService.java
+++ backend/src/main/java/com/MOA/backend/domain/moment/service/MomentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a86f565649dfdc0433371a8226b9f101819637fedfbf9bcdbcf14a9cb50331d
-size 11703
+oid sha256:b1db1a378650284d40d9d2f09935f70ac9ccc136fc8c6643727320d536339d73
+size 12830
