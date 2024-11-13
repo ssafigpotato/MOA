af3f7d3c74498909c1c61687410fe670b551209d
김윤홍 <yunhongkim123@gmail.com>
2024-11-13 20:15:27 +0900

    :fire: [Fix] 그룹 pin번호 난수 생성
    Former-commit-id: 80c9f88fafaace98ab0c6ee50409c8972a199c57

--- backend/src/main/java/com/MOA/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/MOA/backend/domain/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:261e329227f0c1d59c68c4e75e8e0337ff00a1816a22fa804b89ceaf07269d9d
-size 3195
+oid sha256:b32b1f97b2ab409013cb785dcf690f0392282ecabdfb5930510763030772e320
+size 3083
