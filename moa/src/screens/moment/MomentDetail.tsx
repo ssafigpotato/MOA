5122bee474197376aad8a821ead80050a0e57d96
dla9520 <dla9520@naver.com>
2024-11-13 13:50:04 +0900

    :art:[Design] moment 상세 스크린 새로고침 구현
    Former-commit-id: 209df2f7857ecd18185f7afd56a9d12ce310e68c

--- moa/src/screens/moment/MomentDetail.tsx
+++ moa/src/screens/moment/MomentDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6239f42d71d8fcd45690abf8e8f5726b56168f272a191bd1db3d17496a74b25c
-size 3712
+oid sha256:d2e09919d67f956ba6b86ea3de47c75f886d8c4479430cb18b23340fc0d01c68
+size 4196
