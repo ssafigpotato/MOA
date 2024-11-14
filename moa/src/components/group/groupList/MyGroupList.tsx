e1cff6797cd73f4c1ef9e71de37468c1ff0d2af7
dla9520 <dla9520@naver.com>
2024-11-14 13:09:03 +0900

    :sparkles:[Feat] #S11P31A602-171 #in-progress Group List 새로고침 구현
    Former-commit-id: 5118f4710e4ab308314683f6397cd4771cb9e863

--- moa/src/components/group/groupList/MyGroupList.tsx
+++ moa/src/components/group/groupList/MyGroupList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbf30b6415d04e136395d9356ae5e9829c774049430c3f93d978b3d6a5d4f8ab
-size 1444
+oid sha256:cc51ff417a72cc0268341be9b4c0dcd6281687e51068c02a50b96180d4e3ab84
+size 620
