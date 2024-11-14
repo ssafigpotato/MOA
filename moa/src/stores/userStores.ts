0efed03122cf74bd9c683c83ebfffb6a72a24097
inception010203 <inception010203@gmail.com>
2024-11-14 15:10:42 +0900

    :sparkles:[Feat] #S11P31A602-345 #in-progress 얼굴 등록 api함수 userStore에 구현
    Former-commit-id: 13d3988e12501d9604a5d1b122e0e46f08b81082

--- moa/src/stores/userStores.ts
+++ moa/src/stores/userStores.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64ae1ab19e4688733caa5b0b12b90f6507bd95f252258c0b8baff562c798aa5d
-size 3778
+oid sha256:81f8703a0a9a8e418356de62e01e7a8b759d3d19c5e2543ef8a98f0ea0998932
+size 4486
