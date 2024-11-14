481b6f4ead06a2f11797d259fc7b9bc671aa332e
kimjuhyeong <kkjjhh06@naver.com>
2024-11-14 14:01:55 +0900

    :wrench:[Fix] 그룹 가입 로직 및 403 에러 던지기 수정
    Former-commit-id: 0abb26d063eade713d38f32a8784c9aeadc93487

--- backend/src/main/java/com/MOA/backend/domain/group/controller/GroupController.java
+++ backend/src/main/java/com/MOA/backend/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:276177dc81914895a4b7d7bc17fd161d6542daae6612c29d144d2f97254c337e
-size 5973
+oid sha256:f53400f801383ddee539139b02b185fe1edcdaaf1c589668f3f96f028797f686
+size 6141
--- backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
+++ backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d93b9e9ff25a2df16c5bf2493018634c84c9456e1687df1f25273141ea23076
-size 4396
+oid sha256:98c8b175c2607e27faf13914ce83cfdce6056673c5c400b5426eacb926bfdd7d
+size 5209
