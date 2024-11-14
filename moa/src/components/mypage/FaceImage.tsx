dfe45e06870fac8f27d81adfc8bac40f0374811e
inception010203 <inception010203@gmail.com>
2024-11-14 17:58:36 +0900

    :bug:[Fix] #S11P31A602-345 #in-progress 얼굴 등록 api 변경에 따른 로직 수정
    Former-commit-id: 124588aeffbd0e16b38acd3e1a6f782a0c43a8ee

--- moa/src/components/mypage/FaceImage.tsx
+++ moa/src/components/mypage/FaceImage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d711cfb561b96366f14206d1ddc517186fc2a93663bb23f534d7bfa582a43a40
-size 7692
+oid sha256:2fede6182583feee0a0764c0cfd455afc820fed40f78f5535829a6940e3206fe
+size 5946
