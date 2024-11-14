44332ba8b2100d0598d58134d27116b5bef2d4b4
김윤홍 <yunhongkim123@gmail.com>
2024-11-14 13:42:12 +0900

    :sparkles: [Feat] back-feat/S11P31A602-296 그룹 누적 사진
    Former-commit-id: 43c214d6e44435397f39400903d521019561f161

--- backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
+++ backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d93b9e9ff25a2df16c5bf2493018634c84c9456e1687df1f25273141ea23076
-size 4396
+oid sha256:0657f6dcd0d68bbb34c6bf4331859d72421e8ad034e52f9e5fa3d7d3940fcf8b
+size 4902
