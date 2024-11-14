91e7d41e20c9e002e85424a000b185a2ef3b0b41
dla9520 <dla9520@naver.com>
2024-11-14 14:46:23 +0900

    :sparkles:[Feat] #S11P31A602-171 #done 그룹 목록 조회 변수 수정
    Former-commit-id: 5e159fe098b85ab3c1bb86690aa38f282290669b

--- moa/src/components/group/groupList/MyGroupList.tsx
+++ moa/src/components/group/groupList/MyGroupList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:988cf1a4f7c20d2c4d4f7810f16bdadcdc72189228b532347d86410be2b4fe24
-size 2135
+oid sha256:322d20a07f874c8ecbcc506924a1ee7c293c41f4dd8caab56cd129e5ba1abdbc
+size 2152
