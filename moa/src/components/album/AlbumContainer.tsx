4dd8ed203a8e7e532d5a0187f3626c98d9760c96
dla9520 <dla9520@naver.com>
2024-11-15 21:24:33 +0900

    :sparkles:[Feat] #S11P31A602-233 #done 그룹/순간에 따른 PhotoList 변수 변경
    Former-commit-id: eda9549effc9d217c7000d31e71598ba82687745

--- moa/src/components/album/AlbumContainer.tsx
+++ moa/src/components/album/AlbumContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b2253707ca2ea97f2a3bcae486bd003305513e6f8832c7914643138d293e750
-size 8505
+oid sha256:36f2efa613f5a4abaf36d28af8edb1ab072b097dee4e7559cfa634cfb1449f58
+size 8508
--- moa/src/components/album/PhotoList.tsx
+++ moa/src/components/album/PhotoList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80a3caeb0766c4459269abd4c594ffb2b72472e20cf8ab25c5bad537a659cb55
-size 2266
+oid sha256:9cdca450c6106368c1059e4e362bc1c2e584ef5a45a9454c6ca016154794b771
+size 2319
