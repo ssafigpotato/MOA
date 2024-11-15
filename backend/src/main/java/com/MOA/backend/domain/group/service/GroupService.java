ae510cb05a8677a907aaf638f98c87b4edc0e68c
kimjuhyeong <kkjjhh06@naver.com>
2024-11-15 18:20:34 +0900

    [BE-241115] 에러 수정
    Former-commit-id: d4f33412cb9e655b87bf2b2d60d890f6bcf56c02

--- backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
+++ backend/src/main/java/com/MOA/backend/domain/group/service/GroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92abd20ccd56b1e1e5208f2bcf46a4478ff9e40090acfbde593bef7d07065ca1
-size 7229
+oid sha256:4318043f77578b59d83d09ed944a0351197c5e3487940ea94eda472afb3694af
+size 7266
