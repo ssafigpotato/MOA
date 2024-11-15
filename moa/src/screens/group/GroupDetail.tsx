acd839e0c0c80cc13bbf39e5e56fc08fc8a88426
dla9520 <dla9520@naver.com>
2024-11-15 18:16:14 +0900

    :sparkles:[Feat] #S11P31A602-236 #in-progress 그룹 입장 로딩 구현
    Former-commit-id: e8684ef27759b9c2782922c07d557c1e4c8c7901
    Former-commit-id: f55afec27532832c31508612bc62eaad8f6fb58e

--- moa/src/screens/group/GroupDetail.tsx
+++ moa/src/screens/group/GroupDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:745d92412b698c41694d316340396e2f48d43bcc23582a6f37fca94cd3a5d769
-size 5180
+oid sha256:d70931b8af5507b73cc43c68ed1de8beef24ffd3fc7f072d96b713b5b5f41136
+size 5344
