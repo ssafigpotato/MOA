20cfa1f5fe0eb2811d803805a23bb6d1746ec068
kimjuhyeong <kkjjhh06@naver.com>
2024-11-15 18:24:54 +0900

    [BE-241115] 에러 수정
    Former-commit-id: 6fb46571be5e0c5e96f269ed99f59a089b09fe73

--- backend/src/main/java/com/MOA/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/MOA/backend/domain/member/repository/MemberRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5ab4e7c54d45e41d06427e1583984ac518be708700ddfe8200c32c9a0df722f
-size 990
+oid sha256:95f3a2fdfaaf99a3f751d1a562bab45e79e110f27f85ddb1602a3d38fbf70c9a
+size 928
