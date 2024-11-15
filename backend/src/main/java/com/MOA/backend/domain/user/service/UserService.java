fecbed2ec0ff84f9524cb007df02e9376670ee4c
min_yl8234 <min_yl8234@daum.net>
2024-11-15 10:04:58 +0900

    :ambulance:[Fix] 유저 정보 상세조회 ResponseDTO로  수정
    Former-commit-id: 5cd5dd30fa501018b17ce397e745b533abf30b0b

--- backend/src/main/java/com/MOA/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/MOA/backend/domain/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e62acf902f0eb503c30da32dcfe2080451319ba7e4baabc0edd37992b583e03
-size 4417
+oid sha256:756b17a695abff7caf56b8b9314744fe8b37512da57291251934a074cf3fd067
+size 5071
