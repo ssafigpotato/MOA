be90bf00b16e198b1889c4260af59088fb23dbcd
min_yl8234 <min_yl8234@daum.net>
2024-11-15 21:22:19 +0900

    :sparkles:[Feat] #S11P31A602-335 음식 사진 분류
    Former-commit-id: 2dd3890d28d884e4ed6e09026bfbde65339c30b8

--- FastAPI/apis/apis_base.py
+++ FastAPI/apis/apis_base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb612219fd9f0415314e33bd0c0d98de96e01747e57502b101ffcfb0a3606dc6
-size 346
+oid sha256:35add5a754cd21a2cfffba971d03ddffc0a77657a51d0934a3cfb9d37411913f
+size 440
---
+++ FastAPI/apis/detect_food.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:793dbdb4243a262cbc797e0d4cc1f000e8998e45714950fa2c9601fc9f04b9e0
+size 3602
