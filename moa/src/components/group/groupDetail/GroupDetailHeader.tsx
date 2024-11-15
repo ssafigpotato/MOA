be362d5c76f41221875f7b0b23ee672e07239157
dla9520 <dla9520@naver.com>
2024-11-15 15:50:48 +0900

    :sparkles:[Feat] #S11P31A602-303 #in-progress 그룹 삭제 api 개발
    Former-commit-id: bd376c75d7827e175ea66c5c1ef7c3f4be9e7577

--- moa/src/components/group/groupDetail/GroupDetailHeader.tsx
+++ moa/src/components/group/groupDetail/GroupDetailHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70d74fa7fc7314b715c9eb4b6f565baba249ee528d1d58ef296d1d4498dc8320
-size 5548
+oid sha256:ac03e165540720f0b0ab81548f92d38cd61e79883665e4bf4c6e4dc09002f768
+size 6888
