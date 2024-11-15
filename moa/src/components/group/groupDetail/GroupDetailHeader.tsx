107010cfaca2bd99772cc7e4617c236c39ac2016
dla9520 <dla9520@naver.com>
2024-11-15 13:57:11 +0900

    :sparkles:[Feat] #S11P31A602-233 #in-progress 그룹 상세 조회 memberlist 구현 완료
    Former-commit-id: a1bb537fe55350f9c964d19621f7eaf694a714c1

--- moa/src/components/group/groupDetail/GroupDetailHeader.tsx
+++ moa/src/components/group/groupDetail/GroupDetailHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df43e9b0576fa5c12eba1d5fee2bea7a1dfb7ed9a69bf279fe789ca82d6186eb
-size 5214
+oid sha256:14117776aaef24464030bf0606022ce6f532bd20499b3e1ec795860d79a3345a
+size 5296
--- moa/src/components/group/groupList/MyGroupList.tsx
+++ moa/src/components/group/groupList/MyGroupList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:322d20a07f874c8ecbcc506924a1ee7c293c41f4dd8caab56cd129e5ba1abdbc
-size 2152
+oid sha256:a1607fcfa92e0af0410044baaaa5700334f0c1db704d9a4ffdc9937d88c0888a
+size 2173
