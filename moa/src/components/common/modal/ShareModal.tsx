b1eb8b1b56989c7630f616fa320c3f412c49ea0f
dla9520 <dla9520@naver.com>
2024-11-17 02:49:07 +0900

    :art:[Design] #S11P31A602-351 #in-progress 공유 모달 UI 구현
    Former-commit-id: 3eae46ab5fa4b1d7fc91e45835d3d9a1dd60e396

--- moa/src/components/common/modal/ShareModal.tsx
+++ moa/src/components/common/modal/ShareModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a480bba24dd716f46f9588b2c920af8219dade0a0554b66b61e7ed7c81ee07ad
-size 1361
+oid sha256:b384639e6d2822a59449a7a2ac0c9d5286ab498e7065693b32dafdcf1e4115f9
+size 1786
