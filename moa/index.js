5021d3950ffeee3ea6d7fe99538263ad5b31f1b4
dla9520 <dla9520@naver.com>
2024-11-17 04:55:32 +0900

    :art:[Design] Android App 로고 추가
    Former-commit-id: 215913fb1e765cfa099b5e94d32b929c874a8a78

--- moa/index.js
+++ moa/index.js
@@ -1,10 +1,3 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent('moa', () => App);
+version https://git-lfs.github.com/spec/v1
+oid sha256:6eb96ad299601354e3fe671c0f486b57b8f25aa041ec9ca8db5a1552d6bf4783
+size 238
