f412222b319731749044e5e560d906b161ad6d5b
min_yl8234 <min_yl8234@daum.net>
2024-11-17 21:43:10 +0900

    :ambulance:[!HOTFIX] 순간 음식 분류 API 따로 생성
    Former-commit-id: 61758f473f7b302573ae2c28dfcadf7346a51ffa

--- backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
+++ backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a42ff851feff1a71329f3e65c4684d72f7f5fe100e551814177a930eea9bc3cb
-size 16557
+oid sha256:4258b4b79c791ba1a00e29ee608137f8f35aa91a83759f9a91b7e90bd557ca9d
+size 17325
