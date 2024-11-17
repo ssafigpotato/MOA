8a2da96e79a23dace29e5cf7171dffad8254bd4b
inception010203 <inception010203@gmail.com>
2024-11-18 01:26:55 +0900

    :sparkles:[Feat] #S11P31A602-355 #in-progress notification화면 및 알림 로직 구현
    Former-commit-id: d7fb8c14f13c59d455c8112384cf09013ff38763

--- moa/src/screens/Notification.tsx
+++ moa/src/screens/Notification.tsx
@@ -1,15 +1,3 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import ScreenContainer from '../components/common/ScreenContainer';
-
-const Notification = () => {
-  return (
-    <ScreenContainer>
-      <View>
-        <Text>Notification</Text>
-      </View>
-    </ScreenContainer>
-  );
-};
-
-export default Notification;
+version https://git-lfs.github.com/spec/v1
+oid sha256:c02c71bca79897d062edb846ec9fd569991acf6e2c5660f7e037fcd990ab880c
+size 2669
