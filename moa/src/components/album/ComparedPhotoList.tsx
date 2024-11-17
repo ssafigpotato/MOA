e0bcf92757f5f97a0d167545583f7c8cd0355216
dla9520 <dla9520@naver.com>
2024-11-17 23:39:04 +0900

    :bug:[Fix] MomentDetail type 에러 수정
    Former-commit-id: 9b57bcc631109cdfadd97b30c91994c75e10e8e0
    Former-commit-id: c4f59266c474dae6c3cb30f9444d9bf6037c88ba

---
+++ moa/src/components/album/ComparedPhotoList.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b51eed119fcc4444fe3fe79e6c51aa8a14433603253b4d5e3a1bd0c2bfb0ab31
+size 217
--- moa/src/screens/moment/MomentDetail.tsx
+++ moa/src/screens/moment/MomentDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4d2f2f20c9f32434ba62d2e919f4597b399c6ba3bb2ea2ab6d7a57bf9bc301f
-size 4874
+oid sha256:d1495bfa605cbf74de859fd1401509d12d4530f9040431e0319f618156f71bfa
+size 4827
