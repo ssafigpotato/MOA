268a70914defe3cca8108546d2e9f0396bebc6ca
dla9520 <dla9520@naver.com>
2024-11-17 23:43:38 +0900

    :art:[Design] AppHeader 로고 변경
    Former-commit-id: f09be623e53d80357e527554dd8b2c2b602d0f0c

--- moa/src/components/album/ComparedPhotoList.tsx
+++ moa/src/components/album/ComparedPhotoList.tsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b51eed119fcc4444fe3fe79e6c51aa8a14433603253b4d5e3a1bd0c2bfb0ab31
-size 217
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ComparedPhotoList = () => {
+  return (
+    <View>
+      <Text>ComparedPhotoList</Text>
+    </View>
+  );
+};
+
+export default ComparedPhotoList;
