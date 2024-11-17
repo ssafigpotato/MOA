97beb8b409ce2f74896adf2eb68a60e0ca5e6f15
dla9520 <dla9520@naver.com>
2024-11-17 22:34:11 +0900

    :art:[Design] Photo Detail 사진 잘림 오류 해결
    Former-commit-id: 655ab6b4b47c8dc0a1c6b2093822d703c97a4f00
    Former-commit-id: c5c1fa1b446d284f6bf81a37b41599a4c1ecaffa

--- moa/src/screens/PhotoDetail.tsx
+++ moa/src/screens/PhotoDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:934ee472575d7de9c2c5a97d6669b8577cd94a7a46e15dcbb759b3cbbca7ebd6
-size 2129
+oid sha256:8979593bd625795b8bbfb5b3055c358812bff45844113b6342ab4da34fb5c822
+size 2780
