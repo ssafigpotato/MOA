fb31e9c693af1695a091e6d30bb7e4063850bbde
chelsea7023 <yunhongkim123@gmail.com>
2024-11-17 21:24:44 +0900

    :sparkles: [Feat] fcm 로직 수정
    Former-commit-id: 31805ff2e95ad610d035595b55d682ee75f596e2

--- backend/src/main/java/com/MOA/backend/global/config/FCMConfig.java
+++ backend/src/main/java/com/MOA/backend/global/config/FCMConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10fc9744d918951f168d48eb75184ea6859f1bed395ec190b62b41f1aba412ab
-size 1815
+oid sha256:73cc45e5776c074a6e6a95b05d0c9ee8db2dc502bfb7b6370f5267a82dfa299b
+size 2075
