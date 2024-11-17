77b11d12caa851c59e68f6f77616ffadea3612fe
dla9520 <dla9520@naver.com>
2024-11-17 22:44:33 +0900

    :art:[Design] 로그인 화면 애니메이션 적용
    Former-commit-id: e2ed87e16ad84276736e85cf0ecca823a188cdfa

--- moa/src/screens/group/GroupDetail.tsx
+++ moa/src/screens/group/GroupDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc567777edee930850e158caee8c7b5c1a9f25856a327af809b6afc9ce86986e
-size 5304
+oid sha256:fbcbfd88749b633eb32ab7a97f75668ba08728cf187c5b9ceae6c3539b3e19b2
+size 5204
