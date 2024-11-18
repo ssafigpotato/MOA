087a5f30487a94cb5e044f23476ec75e30a1b118
dla9520 <dla9520@naver.com>
2024-11-18 19:55:19 +0900

    :art:[Design] 순간 타이머 UI 수정
    Former-commit-id: e047f2495e3e41c8244de817d47aba11f793c983
    Former-commit-id: b9747f46a040d935b9658cad215849f79cb9fd2f

--- moa/src/components/moment/momentDetail/MomentDetailHeader.tsx
+++ moa/src/components/moment/momentDetail/MomentDetailHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:798eb9244c354590a165bcf3a9750bb3d3c7893429d2d42ae8cd983a146ea9b0
-size 6978
+oid sha256:d838662898e07b1fd336a2ae10d2a319b0eeab1f93da1839bd1a18e6435bf7c7
+size 7596
