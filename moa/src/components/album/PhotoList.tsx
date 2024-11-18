deb41fc9e47197586145e7b55ed6cbed8b05b1e8
dla9520 <dla9520@naver.com>
2024-11-18 21:19:38 +0900

    :art:[Design] 잔여 시간 표시 UI 수정
    Former-commit-id: 2de01e03a268821faf6c53c4120e55e4278471ef
    Former-commit-id: e2f8590c116ae1dc24cb6d372504103717dbe12b

--- moa/src/components/album/PhotoList.tsx
+++ moa/src/components/album/PhotoList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbc58f86089636298bafd58dfadf2d39de926cc27da1cbd5aa78ce7001437143
-size 3334
+oid sha256:0be8db9615625d30e1e5c5fcb82940acc72ed6c73f92fe0a0f34d59745fbdbe5
+size 3287
