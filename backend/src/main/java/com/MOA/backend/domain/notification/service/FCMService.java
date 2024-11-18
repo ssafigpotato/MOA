6387977b6699f5cf97ab41b894506cf240110afd
김윤홍 <yunhongkim123@gmail.com>
2024-11-18 20:02:01 +0900

    :sparkles: [Feat] back-feat fcm수정
    Former-commit-id: 1a9d3921e0f6cf63eb7865fea6602a4b17dbdc64

--- backend/src/main/java/com/MOA/backend/domain/notification/service/FCMService.java
+++ backend/src/main/java/com/MOA/backend/domain/notification/service/FCMService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e04c312797d6f9836679f2f5cd7c5b5a77dacd55230ba20255e8aa40a4cd812
-size 7394
+oid sha256:ac7db7b190342e6722c95d57b410d591038026b18fa951e9041ecac661cac920
+size 7272
