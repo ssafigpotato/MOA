3ba3d7075a9834db9a200346056cc4ef6ec92c73
kimjuhyeong <kkjjhh06@naver.com>
2024-11-18 14:00:43 +0900

    :ambulance:[!HOTFIX] Batch Test
    Former-commit-id: 1deefb138e0b3bb984713c8e2deae753fbdae0f5

--- backend/src/main/java/com/MOA/backend/global/batch/scheduler/BatchSchedulerImpl.java
+++ backend/src/main/java/com/MOA/backend/global/batch/scheduler/BatchSchedulerImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9735180d6bf4ccd6fd1e01d8f09f7223ddac526c7a6e496019106379dd094b32
-size 1947
+oid sha256:bee7882632b04b4061fde2658f2e8fdb5ec25029e2872da5056db33774d069a6
+size 1946
