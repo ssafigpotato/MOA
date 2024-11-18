66c0f54b2587069c401ede8d7523a729c4fd60a7
kimjuhyeong <kkjjhh06@naver.com>
2024-11-18 11:07:08 +0900

    :wrench:[Fix] #S11P31A602-361 이미지 업로드 예외처리 세부 설정
    Former-commit-id: 50c107d5de81d9a2f1267aaf5b589f027ab28096

--- backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
+++ backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7b3109a8fe231ec38c9da75c4d3bf52527a410c11bf55bc58feb1290a654d34
-size 17188
+oid sha256:a0bb08bf19e35169138db7e83da6e7c3e7a232e57970708f754a9fdb2ad0b9d6
+size 18991
