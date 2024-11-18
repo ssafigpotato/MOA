5f2232dd71708ca1bfde5c2f9c5b15cf4abb99de
김윤홍 <yunhongkim123@gmail.com>
2024-11-18 10:42:42 +0900

    :sparkles: [Feat] back-feat fcm
    Former-commit-id: 47947727d9acb39705bfe553845f0903b0d19960

--- backend/src/main/java/com/MOA/backend/global/config/FCMConfig.java
+++ backend/src/main/java/com/MOA/backend/global/config/FCMConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b35246c39e0cbd8afb118888dd8b2db00c7d65fb7165a61bbd10dfa7f7d8bfc
-size 2205
+oid sha256:01c7ae592ab5ff9759927ea38376dadffd48767a9879ee7ea90dee7643743889
+size 2265
