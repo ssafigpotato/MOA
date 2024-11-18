d30155b846116b8cf0e6f53aa565962e0948a645
kimjuhyeong <kkjjhh06@naver.com>
2024-11-18 13:49:49 +0900

    :ambulance:[!HOTFIX] 핫픽스
    Former-commit-id: e16a465f7a03e40d8dc9e96e318e2c5adebec343

--- backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
+++ backend/src/main/java/com/MOA/backend/domain/image/service/S3Service.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c135a6db64fe1b4bd0182eecd1df4978e749af81a8c476bbcc93a6587d8d539d
-size 19005
+oid sha256:746c295b6ba9bc3c24de1b204eea5636133910527db4e3733e691a5953e4bef7
+size 19003
