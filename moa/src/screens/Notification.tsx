8beb6f40db6bfdb59701a25a03f789287b770a5c
ssafigpotato <ssaf010203@gmail.com>
2024-11-18 13:40:23 +0900

    :art:[Design] #S11P31A602-355 #in-progress 알림관련 CSS 및 아이콘 수정
    Former-commit-id: 820e9028a68d0a477c9c187b8a7d2ee642176777
    Former-commit-id: 65f2f1b2987c8feac66d0ecb5eb8add81a33fe76

--- moa/src/screens/Notification.tsx
+++ moa/src/screens/Notification.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87cb692cd996c804a5b4603d08f8aeaabceaf6cae0931503cebc9ff2296c22cc
-size 3720
+oid sha256:5d792f21ecdd784a4eb6871e1aa113240b5f922a2cc86e13a90d3022a7a289b7
+size 3722
--- moa/src/utils/FirebaseSettings.ts
+++ moa/src/utils/FirebaseSettings.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e15bbc3f0688af8d38d5131fd735656699ddfd2aa9399bb08be0cd6264a1062f
-size 4439
+oid sha256:e7aba9483565c256c4c46624b7eac4ef9a56f02be258f3b8bf541293683ba326
+size 4442
