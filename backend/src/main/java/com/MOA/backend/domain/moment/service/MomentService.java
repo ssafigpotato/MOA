1517f57dbc2588dff892d5391df3c8a0b4947e52
kimjuhyeong <kkjjhh06@naver.com>
2024-11-19 09:27:59 +0900

    [HOTFIX] 사진 순서 정렬
    Former-commit-id: 7b8b5df7e01211d8ff0c6238764f5e8f53acae82

--- backend/src/main/java/com/MOA/backend/domain/moment/service/MomentService.java
+++ backend/src/main/java/com/MOA/backend/domain/moment/service/MomentService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09082f1afde6d3d0efd6d9c76e2269ceee2e0d72bb47482ad6b43a9b68e23872
-size 14670
+oid sha256:db5928faaf35baf484ae274ce46460929058bcb690e8ebe9916692c609c46855
+size 14689
